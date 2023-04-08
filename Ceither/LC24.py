7b717d0eedfb83083d146027fb5b0da24a5b4fdb
ceither <z5437960@ad.unsw.edu.au>
2023-04-08 15:09:54 +1000

    1

--- Ceither/LC24.py
+++ Ceither/LC24.py
@@ -1,11 +1,16 @@
-def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    if head is None:
-        return None
-    if head.next == None:
-        return head
-    p = head
-    q = head.next
-    p.next = self.swapPairs(q.next)
-    q.next = p
-    return q
-swapPairs()
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        if head.next == None:
+            return head
+        p = head
+        q = head.next
+        p.next = self.swapPairs(q.next)
+        q.next = p
+        return q
