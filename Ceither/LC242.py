8bbfd1cf6d9f67a07426ac17a15a196da8ae2cc5
ceither <z5437960@ad.unsw.edu.au>
2023-04-12 20:43:36 +1000

    To B or Not To B?

---
+++ Ceither/LC242.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash_table = {}
+        for i in s:
+            if hash_table.get(i):
+                hash_table[i] += 1
+            else:
+                hash_table[i] = 1
+        for i in t:
+            if hash_table.get(i):
+                hash_table[i] -= 1
+            else:
+                return False
+        return True
