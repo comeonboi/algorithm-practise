12db4cab370679ab3e9d74f3f5f254a3612b8973
ceither <z5437960@ad.unsw.edu.au>
2023-05-17 20:03:23 +0800

    To B or Not To B?

--- Ceither/LC242.py
+++ Ceither/LC242.py
@@ -1,16 +1,18 @@
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        hash_table = {}
-        for i in s:
-            if hash_table.get(i):
-                hash_table[i] += 1
-            else:
-                hash_table[i] = 1
-        for i in t:
-            if hash_table.get(i):
-                hash_table[i] -= 1
-            else:
-                return False
-        return True
+        # if len(s) != len(t):
+        #     return False
+        # hash_table = {}
+        # for i in s:
+        #     if hash_table.get(i):
+        #         hash_table[i] += 1
+        #     else:
+        #         hash_table[i] = 1
+        # for i in t:
+        #     if hash_table.get(i):
+        #         hash_table[i] -= 1
+        #     else:
+        #         return False
+        # return True
+        return Counter(s) == Counter(t) #真得一行吧
