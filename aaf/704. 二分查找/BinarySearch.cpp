781160ada72afa394e5d0c944a0a58a9fc50c686
XIAOCHENG-LEE <lixiaocheng965@gmail.com>
2023-06-15 17:53:47 +1000

    test

---
+++ aaf/704. 二分查找/BinarySearch.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Xiaocheng Li on 8/4/2023.
+//
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left <= right){
+            int mid = (right + left) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+};
