4f680e5491c5d95d58b59b7180b3d3bb7a69f16f
luyuquan <z5434264@gmail.com>
2023-06-26 00:07:05 +1000

    6月26日泡芙

---
+++ 泡芙代码随想录/第二十二天/701. 二叉搜索树中的插入操作.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            node = TreeNode(val)
+            return node
+
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+
+        return root
