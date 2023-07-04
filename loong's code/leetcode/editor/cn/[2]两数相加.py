8f84d8850775dd235cc59b849015c3d93a481154
Sizhuo Long <longsizhuo@gmail.com>
2023-07-04 23:05:46 +1000

    第24天龙龙

--- loong's code/leetcode/editor/cn/[2]两数相加.py
+++ loong's code/leetcode/editor/cn/[2]两数相加.py
@@ -46,51 +46,46 @@ from typing import Optional
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
-        # if l1 is None and l2 is None:
-        #     return ListNode(carry) if carry else None
-        # if l1 is None:
-        #     l1, l2 = l2, l1
-        # carry += l1.val + (l2.val if l2 else 0)
-        # l1.val = carry % 10
-        # l1.next = self.addTwoNumbers(l1.next, l2.next if l2 else None, carry//10)
-        # return l1
-        l1_list = []
-        l2_list = []
-
-        def recursion_list(node: ListNode, listt):
-            if not node:
-                return listt
-            listt.append(node.val)
-            return recursion_list(node.next, listt)
-
-        l1_list = recursion_list(l1, l1_list)
-        l2_list = recursion_list(l2, l2_list)
-        l1_list = ''.join(map(str, l1_list))
-        l2_list = ''.join(map(str, l2_list))
-        l3 = str(int(l1_list) + int(l2_list))
-        print(l3)
-
-        def recursion_linkArray(num):
-            if not num:
-                return None
-            head = ListNode(0)
-            current = head
-            for c in num:
-                current.next = ListNode(int(c))
-                current = current.next
-            return head.next
-
-        head = recursion_linkArray(l3[::-1])
-        print(recursion_list(head, []))
-        print(type(l1), type(head))
-        return head
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry = 0) -> Optional[ListNode]:
+        if l1 is None and l2 is None:
+            return ListNode(carry) if carry else None
+        if l1 is None:
+            l1, l2 = l2, l1
+        carry += l1.val + (l2.val if l2 else 0)
+        l1.val = carry % 10
+        l1.next = self.addTwoNumbers(l1.next, l2.next if l2 else None, carry//10)
+        return l1
+        # l1_list = []
+        # l2_list=[]
+        # def recursion_list(node:ListNode, listt):
+        #     if not node:
+        #         return listt
+        #     listt.append(node.val)
+        #     return recursion_list(node.next, listt)
+        # l1_list = recursion_list(l1, l1_list)
+        # l2_list = recursion_list(l2, l2_list)
+        # l1_list = ''.join(map(str, l1_list))
+        # l2_list = ''.join(map(str, l2_list))
+        # l3 = str(int(l1_list)+int(l2_list))
+        # print(l3)
+        # def recursion_linkArray(num):
+        #     if not num:
+        #         return None
+        #     head = ListNode(0)
+        #     current = head
+        #     for c in num:
+        #         current.next = ListNode(int(c))
+        #         current = current.next
+        #     return head.next
+        # head = recursion_linkArray(l3[::-1])
+        # print(recursion_list(head, []))
+        # print(type(l1), type(head))
+        # return head
 
 # leetcode submit region end(Prohibit modification and deletion)
